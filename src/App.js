a9473b775ffe16f5f38dd16046feea61344f378b
Amit Goyal <agoyal3_be21@thapar.edu>
2022-07-04 11:50:14 +0530

    Random scribbles using threeJS

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import {Fragment} from 'react';
 import './App.css';
+import Scribble from './Components/Random line/Scribble';
 import Line from './Components/SImple line/Line';
-
 function App () {
   return (
     <Fragment>
       <Line />
+      <Scribble />
     </Fragment>
   );
 }
